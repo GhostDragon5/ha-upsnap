929419dae769f5187b3ef73e873a9e6540906578
GhostDragon <Nicoimholz@gmail.com>
2026-04-30 01:45:04 +0200

    debug

--- custom_components/ha-upsnap/_init_.py
+++ custom_components/ha-upsnap/_init_.py
@@ -1,7 +1,3 @@
-# custom_components/upsnap/__init__.py  ← MINIMALE TESTVERSION
-
-from __future__ import annotations
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
@@ -9,12 +5,10 @@ DOMAIN = "upsnap"
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    hass.data.setdefault(DOMAIN, {})
     return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.data.setdefault(DOMAIN, {})
     return True
 
 
